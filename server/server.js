601f8ef3381d9a014eead7c823a930569d6ad54b
kguinto <klogold1@gmail.com>
2018-03-30 15:36:38 -0700

    Implemented lightweight server to serve data from mongo

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 80;
+
+MongoClient.connect('mongodb://localhost/descriptions')
+  .then((mongoClient) => {
+    app.use(cors());
+
+    const db = mongoClient.db('descriptions');
+    const collection = db.collection('descriptions');
+
+    app.get('/:id', async (req, res) => {
+      const { id } = req.params;
+      let itemData = await getDescriptionById(collection, id);
+
+      res.send(itemData);
+    });
+
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`);
+    });
+  });
